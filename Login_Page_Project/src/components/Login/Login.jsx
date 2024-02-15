f120ce62becb91bcfc72716f6904912d235df9f9
Arth H. Patel <arth.patel@bypeopletechnologies.com>
2024-02-15 11:44:10 +0530

    login page work

--- Login_Page_Project/src/components/Login/Login.jsx
+++ Login_Page_Project/src/components/Login/Login.jsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import './Login.css'
+import React, { useState } from "react";
+import usersData from "../../data/Login_data.js";
+import "./Login.css";
 
 const Login = () => {
-  return (
-    <div className="form_body" >
-    <div className="login_container">
-    <form className="dark_form">
-        <label htmlFor="email">User Email</label><br />
-        <input type="email" id="email" /><br />
-        <label htmlFor="password">Pass Word</label>
-        <input type="password" id="password" />
-        <button>
-            Log In
-        </button><br/>
-        <label>Don't have an Account?</label>
-        <a>Singnup Now</a>
-    </form>
-    </div>
-    </div>
-  )
-}
+  const [userEamil, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
 
-export default Login
+  const handleSetEmail = (event) => {
+    setUserEmail(event.target.value);
+    console.log(userEamil);
+  };
+  const handleSetPassword = (event) => {
+    setUserPassword(event.target.value);
+    console.log(userPassword);
+  };
+  const handleUserLogin = () => {
+    try{
+    const userDetails = usersData.map(
+      (user) => user.useremail === userEamil && user.password === userPassword
+    );
+    console.log(userDetails)
+    if (userDetails) {
+      console.log('welcome');
+    } else {
+      alert('Invalid. Please try again.');
+    }
+  }catch(error){console.log(error.message())}
+        // console.log();
+  };
+  return (
+    <div className="form_body">
+      <div className="login_container">
+        <form className="dark_form" onSubmit={handleUserLogin}>
+          <label htmlFor="email">User Email</label>
+          <br />
+          <input
+            type="email"
+            id="email"
+            value={userEamil}
+            onChange={handleSetEmail}
+          />
+          <br />
+          <label htmlFor="password">Pass Word</label>
+          <br />
+          <input
+            type="password"
+            id="password"
+            value={userPassword}
+            onChange={handleSetPassword}
+          />
+          <br />
+          <button>Log In</button>
+          <br />
+          <label>Don't have an Account?</label>
+          <a>Singnup Now</a>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ Login_Page_Project/src/data/Login_data.js
@@ -0,0 +1,7 @@
+const users = [
+    { useremail: 'arth.patel@bypeopletechnologies.com', password: '1234' },
+    { usereamil: 'arth12@gmail.com', password: '1234' },
+  ];
+  
+  export default users;
+  
