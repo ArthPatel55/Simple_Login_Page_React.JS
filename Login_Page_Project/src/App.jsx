c80109a7c4efb8fa62a320559842b585ff47c922
Arth H. Patel <arth.patel@bypeopletechnologies.com>
2024-02-17 16:48:12 +0530

    Revert "completed projects"
    This reverts commit 7bce966565327c4597529b5d5e5e59f9b2be6231.

--- Login_Page_Project/src/App.jsx
+++ Login_Page_Project/src/App.jsx
@@ -3,7 +3,6 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import OtpPage from "./components/otppage/OtpPage.jsx";
 import Signup from "./components/Signup/Signup.jsx";
 import Dashboard from "./components/Dashboard/Dashboard.jsx";
-import AddUser from "./components/AddUser/AddUser.jsx";
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
             <Route exact path="/otppage" element={<OtpPage/>}/>
             <Route exact path="/signup" element={<Signup/>} />
             <Route exact path="/dashboard" element={<Dashboard/>} />
-            <Route exact path="/adduser" element={<AddUser/>}/>
           </Routes>
         </Router>
     </>
